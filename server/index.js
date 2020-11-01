36ef03dd39c41445fad0654eca335c1309fd8911
Jorge <jorgediazok@gmail.com>
2020-11-01 12:42:48 -0300

    db is connected with mongo

--- server/index.js
+++ server/index.js
@@ -2,3 +2,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+//Set app
+const app = express();
+
+//Middlewares
+
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+//MongoDB
+
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((db) => console.log('DB is connected!!'))
+  .catch((err) => console.log(err));
+
+//Server
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
