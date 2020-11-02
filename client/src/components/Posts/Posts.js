13f15c2447d329236f50eb8de2db2a67134e57dd
Jorge <jorgediazok@gmail.com>
2020-11-01 23:23:24 -0300

    created components  , imported them into app and used material-ui

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post/Post';
+
+function Posts() {
+  return (
+    <React.Fragment>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+    </React.Fragment>
+  );
+}
+
+export default Posts;
