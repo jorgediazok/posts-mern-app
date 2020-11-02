4f7d6d315420d6a24d66c77124353268a37f605f
Jorge <jorgediazok@gmail.com>
2020-11-02 00:11:11 -0300

    added styled components to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,27 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+// @ts-ignore
 import memories from './images/memories.png';
 
+//Styles
+import useStyles from './styles';
+
 function App() {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center" className={classes.heading}>
           Memories
         </Typography>
-        <img src={memories} alt="memories" heigth="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
