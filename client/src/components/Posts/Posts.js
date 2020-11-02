65fcbe8a3ec06f41e2043b14f75cc0c114b6e7bf
Jorge <jorgediazok@gmail.com>
2020-11-02 00:48:26 -0300

    added more styles and created api url

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import useStyles from './styles';
 import Post from './Post/Post';
 
 function Posts() {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <h1>Posts</h1>
