57a4e54993c785e4c1cdbfcb48b797ae018f9cbc
Jorge <jorgediazok@gmail.com>
2020-11-06 14:53:29 -0300

    created logic in posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 import useStyles from './styles';
 import Post from './Post/Post';
 
 function Posts() {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
-  return (
-    <React.Fragment>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </React.Fragment>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}>
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
