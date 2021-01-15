26e2b13e6dfa9b6da20f8cc9e15da560ed982e2d
Jorge <jorgediazok@gmail.com>
2021-01-15 17:54:20 -0300

    start from here the login

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,48 @@
-import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
-import Home from './Pages/Home';
-import SignUp from './Pages/SignUp';
-import Login from './Pages/Login';
-//This is a comment
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+import memories from './images/memories.png';
 
-function App() {
-  const [isLogged, setIsLogged] = useState(true);
+const App = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
 
   return (
-    <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/signup">
-            <SignUp />
-          </Route>
-          {!isLogged && <Redirect to="/login" />}
-          <Route exact path="/home">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </React.Fragment>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
-}
+};
 
 export default App;
