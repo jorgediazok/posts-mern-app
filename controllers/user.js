296d342482e1da02bed382e80774c4eceb0f8138
Jorge <jorgediazok@gmail.com>
2021-01-16 13:52:52 -0300

    signin controller logic

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,43 @@ import User from '../models/user';
 
 export const signin = async (req, res) => {
   const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({
+        message: 'User does not exist',
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: 'Invalid Credentials',
+      });
+    }
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      'test',
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({
+      result: existingUser,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Something went wrong',
+    });
+  }
 };
 
-export const signup = async (req, res) => {};
+export const signup = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+};
