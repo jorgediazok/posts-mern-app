4b2b1f428caabc93cd2326f6fff1613e8db67b71
Jorge <jorgediazok@gmail.com>
2021-01-17 00:00:24 -0300

    all functionalities of app working. CSS LEFT

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -36,19 +36,21 @@ export const updatePost = (id, post) => async (dispatch) => {
   }
 };
 
-export const deletePost = (id) => async (dispatch) => {
+export const likePost = (id) => async (dispatch) => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   try {
-    await api.deletePost(id);
-    dispatch({ type: DELETE, payload: id });
+    const { data } = await api.likePost(id, user?.token);
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const likePost = (id) => async (dispatch) => {
+export const deletePost = (id) => async (dispatch) => {
   try {
-    const { data } = await api.likePost(id);
-    dispatch({ type: LIKE, payload: data });
+    await api.deletePost(id);
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -19,10 +19,10 @@ API.interceptors.request.use((req) => {
 
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts', newPost);
-export const likePost = (id) => axios.put(`/posts/${id}/likePost`);
+export const likePost = (id) => API.put(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) =>
   API.put(`/posts/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`/posts/${id}`);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
 
 export const signIn = (formData) => API.post(`/user/signin`, formData);
 export const signUp = (formData) => API.post(`/user/signup`, formData);
