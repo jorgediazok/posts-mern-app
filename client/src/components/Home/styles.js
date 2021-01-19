9b9247558bee9a0738977da1b7b48db450b945ee
Jorge <jorgediazok@gmail.com>
2021-01-18 22:14:03 -0300

    one more try

--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -2,6 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   container: {
+    backgroundAttachment: 'fixed',
+    backgroundSize: 'cover',
     [theme.breakpoints.down('xs')]: {
       display: 'flex',
       flexDirection: 'column-reverse',
