b2dd8d7a201aadf65435745d48ae7342652c1fc4
Jorge <jorgediazok@gmail.com>
2021-01-19 16:00:37 -0300

    changed resolve for join

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use(express.static('client/build/'));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 //Using routes (all routes will start with posts)
