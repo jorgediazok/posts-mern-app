c4a0d71102be0147e9994d95d3e5a506fc8b14dd
Jorge <jorgediazok@gmail.com>
2021-01-19 21:31:59 -0300

    added hashrouter

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,16 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
   document.getElementById('root')
 );
 
