bdd2d48a203d95cfe7e3b38a6be3c544a37979a7
Jorge <jorgediazok@gmail.com>
2021-05-21 00:37:32 -0300

    added searchbar

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -18,10 +18,19 @@ import useStyles from './styles';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 const Home = () => {
+  const classes = useStyles();
+
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
-  const classes = useStyles();
+  const query = useQuery();
+  const history = useHistory();
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery');
 
   useEffect(() => {
     dispatch(getPosts());
@@ -29,17 +38,30 @@ const Home = () => {
 
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
           container
           justify="space-between"
           className={classes.container}
           alignItems="stretch"
           spacing={3}>
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit">
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value="TEST"
+                onChange={() => {}}
+              />
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
               <Pagination />
